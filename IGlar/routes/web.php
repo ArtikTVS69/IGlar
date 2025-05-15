3a59b2d3cc344a5364203fe8bbf1da3944c1666e
Peter Haník <165880359+LosPetros@users.noreply.github.com>
2025-05-15 08:45:05 +0200

    auto login page

--- IGlar/routes/web.php
+++ IGlar/routes/web.php
@@ -5,9 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController; // Intelephense doesn't find this
 
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-})->name('welcome');
+})->name('welcome');*/
+Route::get('/', function () {
+    return redirect()->route('login');
+})->name('home');
 
 Route::get('/profile', function(){
     return view('profile');
